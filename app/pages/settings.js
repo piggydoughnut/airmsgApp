91f4160803955e03a85c41823078a36352945bec
Daria Mikhailova <dariam89@mail.ru>
2016-03-13 18:23:16 +0100

    added myMessages, profile, settings pages templates

---
+++ app/pages/settings.js
@@ -0,0 +1,18 @@
+var React = require('react-native');
+var {
+    Appregistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    } = React;
+
+var styles = StyleSheet.create({
+
+});
+
+class Settings extends React.Component {
+
+}
+
+module.exports = Settings;
