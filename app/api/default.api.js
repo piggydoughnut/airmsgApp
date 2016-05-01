792329e5be9bbd4d7a7f2608972b19ea9e6802da
Daria Mikhailova <dariam89@mail.ru>
2016-05-01 20:34:39 +0200

    api requests refactor

--- app/api/default.api.js
+++ app/api/default.api.js
@@ -28,4 +28,44 @@ export function checkResponseStatus(response) {
     }
 }
 
+export function get(url, token){
+    return request(url, 'GET', token);
+}
+
+export function post(url, token, data){
+    return request(url, 'POST', token, data);
+}
+
+export function request(url, method, token, data){
+    var request_body = {
+        method: method,
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    };
+    if(data && data !== undefined){
+        request_body.body = JSON.stringify(data);
+    }
+
+    return fetch(url, request_body)
+        .then((response) => {
+            console.log(response);
+            if (response.status >= 400) {
+                return {
+                    status: response.status
+                }
+            }
+            return response.json();
+        })
+        .then((responseData) => {
+            return responseData;
+        })
+        .catch((err) => {
+            return {
+                error: 'Network request failed'
+            }
+        });
+}
+
 
--- app/api/messages.api.js
+++ app/api/messages.api.js
@@ -1,72 +1,34 @@
 /** API Calls to /messages */
 var api = require('../config/api');
-import {checkStatus, errorModal} from "./default.api";
+import {checkStatus, get} from "./default.api";
 var React = require('react-native');
+
+export function loadMessages(data) {
+    return get(api.domain + "/messages?lng=" + data.position.longitude + '&lat=' + data.position.latitude, data.token)
+}
+
+export function loadComments(data) {
+    return get(api.domain + "/messages/" + data.id + '/comments', data.token);
+}
+
+export function openMessage(data) {
+    return get(api.domain + "/messages/" + data.id, data.token);
+}
+
 
 export function postMessage(data) {
     return fetch(api.domain + "/messages", {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + data.token
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data.data)
     })
         .then(checkStatus)
         .then((response) => response.json())
         .then((responseData) => {
             return responseData;
-        });
-}
-
-export function loadMessages(data) {
-    return fetch(api.domain + "/messages?lng=" + data.position.longitude + '&lat=' + data.position.latitude, {
-        method: "GET",
-        headers: {
-            'Authorization': 'Bearer ' + data.token
-        }
-    })
-        .then((response) => {
-            console.log(response);
-            if (response.status >= 400) {
-                return {
-                    status: response.status
-                }
-            }
-            return response.json();
-        })
-        .then((responseData) => {
-            return responseData
-        })
-        .catch(error => {
-            return {
-                error: 'Network request failed'
-            }
-        });
-}
-
-export function openMessage(data) {
-    return fetch(api.domain + "/messages/" + data.id, {
-        method: "GET",
-        headers: {
-            'Authorization': 'Bearer ' + data.token
-        }
-    })
-        .then((response) => {
-            console.log(response);
-            if (response.status >= 400) {
-                return {
-                    status: response.status
-                }
-            }
-            return response.json();
-        })
-        .then((responseData) => {
-            return responseData
-        })
-        .catch(error => {
-            return {
-                error: 'Network request failed'
-            }
         });
 }
 
@@ -83,16 +45,3 @@ export function postComment(comment) {
             return JSON.stringify(responseData);
         });
 }
-
-export function loadComments(data) {
-    return fetch(api.domain + "/messages/" + data.id + '/comments',
-        {method: "GET"})
-        .then((response) => response.json())
-        .then((responseData) => {
-            return responseData
-        });
-}
-
-/**
- * @todo: handle HTTP errors
- */
