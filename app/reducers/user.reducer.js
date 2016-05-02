d25b13b38146de13d11494d5f0a9d26e3dc9f286
Daria Mikhailova <dariam89@mail.ru>
2016-05-02 11:52:45 +0200

    loading more messages

--- app/reducers/user.reducer.js
+++ app/reducers/user.reducer.js
@@ -1,5 +1,5 @@
-import {LOGIN_SUCCESS, LOGIN_FAILURE, SET_TOKEN } from '../actions/auth.actions.js'
-import {MESSAGES_USER_LOAD_SUCCESS, MESSAGES_USER_LOAD_FAILURE } from '../actions/messages.actions.js'
+import {LOGIN_SUCCESS, LOGIN_FAILURE, SET_TOKEN} from "../actions/auth.actions.js";
+import {MESSAGES_USER_LOAD_SUCCESS, MESSAGES_USER_LOAD_FAILURE} from "../actions/messages.actions.js";
 
 // initial state for login is empty user details
 const user = (state = [], action) => {
@@ -18,12 +18,32 @@ const user = (state = [], action) => {
                 user: action.payload
             };
         case MESSAGES_USER_LOAD_SUCCESS:
-            return{
-                ...state,
-                messages: action.payload.messages
+            var messagesDocs = [];
+            console.log(state.messages === undefined );
+            console.log(state.messages === [] );
+            console.log(action.payload.messages.offset === 0);
+            if (state.messages === undefined || state.messages === [] || action.payload.messages.offset === 0) {
+                messagesDocs = action.payload.messages.docs;
+            } else {
+                action.payload.messages.docs.forEach(function(val){
+                    state.messages.docs.push(val);
+                });
+                messagesDocs = state.messages.docs;
+            }
+            console.log(messagesDocs);
+            return {
+                tokenInfo: state.tokenInfo,
+                user: state.user,
+                messages: {
+                    docs: messagesDocs,
+                    total: action.payload.messages.total,
+                    offset: action.payload.messages.offset,
+                    limit: action.payload.messages.limit
+
+                }
             };
         case MESSAGES_USER_LOAD_FAILURE:
-            return{
+            return {
                 ...state,
                 error: action.payload.error
             };
